59b21d3403c2a434f61640b4ac9a9f5675301d24
Azzemeen <Azzemeen@gmx.de>
2012-06-14 23:23:00 +0300

    Update master

--- src/Signal.java
+++ src/Signal.java
@@ -1,19 +1,20 @@
+package Stufe2;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Signal {
 	private String signalName;
 	private boolean value;
-	private ArrayList<Nand> spannergatter; //= new ArrayList();;
+	private ArrayList<Nand> spannergatter; // = new ArrayList();;
 
 	public Signal(String signalName) {
 		this.signalName = signalName;
 		value = false;
-		spannergatter = new ArrayList();
+		spannergatter = new ArrayList<Nand>();
 	}
 
 	public void speichern(Nand name)// Die Arrayliste mit Spannergattern
-									// bef�llen
+	// befüllen
 	{
 		spannergatter.add(name);
 	}
@@ -25,19 +26,23 @@ public class Signal {
 	public void setValue(boolean wert) {
 		value = wert;
 		if (spannergatter.isEmpty()) {
-			System.out.println("Wert von" + signalName + " ist: " + value);
+			System.out.println("Wert von " + signalName + " ist: " + value);
 		} else {
 			for (Nand gatter : spannergatter) { // in the arraylist. Add a gate
 				gatter.berechne(); // by calling callMe()
 			}
 		}
 	}
-
+	
+	public String getName(){
+		return signalName;   //Wird von der methode getName in Event aufgerufen, welche von system.out.println in Timingsimulator aufgeruffen wird
+	}
+	
 	public void setValue(boolean wert, int time) {
-		//spannergatter = new ArrayList<Nand>();
+		// spannergatter = new ArrayList<Nand>();
 		value = wert;
 		if (spannergatter.isEmpty()) {
-			System.out.println("Wert von" + signalName + " ist: " + value);
+			System.out.println("Wert von " + signalName + " ist: " + value + " Zeit: " + time);
 		}
 
 		else {
