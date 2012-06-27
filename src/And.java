a3c25906dbd651eb7f6b75301fbb2849bd5752a0
Azzemeen <Azzemeen@gmx.de>
2012-06-27 15:29:32 +0300

    Update master

--- src/And.java
+++ src/And.java
@@ -1,12 +1,36 @@
+package Stufe3;
 
 public class And extends Gatter {
 
 	public And(int eingänge, int delay) {
 		super(eingänge, delay);
-		// TODO Auto-generated constructor stub
-	}
-	public void berechne() {
-		
 	}
 
+	public void berechne() {
+		boolean ergebnis = true;
+		for (Signal s : eingangssignale) {
+			ergebnis = ergebnis & s.getValue();
+		}
+		//ergebnis = ergebnis;
+		if (ergebnis != ausgangssignal.getValue()) {
+			ausgangssignal.setValue(ergebnis);
+		}
+	}
+
+	public void berechne(int t) {
+		boolean ergebnis = true;
+		for (Signal s : eingangssignale) {
+			ergebnis = ergebnis & s.getValue();
+		}
+		//ergebnis = ergebnis;
+		{
+			if (ergebnis != ausgangssignal.getValue()) { // nur ein event
+															// erzeugen, wenn
+															// sich der wert
+															// geändert hat
+				new Event(ausgangssignal, t += delay, ergebnis);
+			} else { // System.out.println("keine änderung");
+			}
+		}
+	}
 }
